afcc6b72c084301b6d844caad3929c1b52c2de4c
JuanMercurio <juanmercuriobadii@gmail.com>
2024-06-07 14:24:00 -0300

    Initial Commit

---
+++ src/main/java/utn/testGraphQL/TestGraphQlApplication.java
@@ -0,0 +1,13 @@
+package utn.testGraphQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGraphQlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utn/testGraphQL/TestGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.testGraphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
